d43798fdb04478e91bc442aab295de1eb271f508
dangpro54321 <dangnhph09124@fpt.edu.vn>
2020-03-28 18:06:35 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# asmm-haidang
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/asmm-haidang)
---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { LoginComponent } from './login/login.component';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { SlideComponent } from './slide/slide.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, LoginComponent, ProductComponent, ProductListComponent, SlideComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
